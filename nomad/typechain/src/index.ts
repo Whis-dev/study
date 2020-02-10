db4d5f5b46ab44a96ba84684abdcb536a2cdef4a
whis <whisdevj@gmail.com>
2020-02-10 15:27:13 +0900

    [nomad>typechain] nomad coders typescript free course #7

--- nomad/typechain/src/index.ts
+++ nomad/typechain/src/index.ts
@@ -1,20 +1,28 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age:number, gender: string) {
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previouseHash: string,
+        data: string,
+        timestamp: number
+    ) {
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previouseHash;
+        this.data = data;
+        this.timestamp = timestamp
     }
 }
 
-const whis = new Human("whis", 32, "unknown")
+const genesisBlock: Block = new Block(0, "20200202020", "", "Hello", 123456);
 
-const sayHi = (person: Human):void => {
-    console.log(`Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`);
-};
+let blockchain:[Block] = [genesisBlock];
 
-sayHi(whis);
+console.log(blockchain);
 
 export { };
