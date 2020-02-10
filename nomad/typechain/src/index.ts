175eb0d05627cab538e44c42debe8d5eabe2385c
whis <whisdevj@gmail.com>
2020-02-10 15:55:08 +0900

    [nomad>typechain] nomad coders typescript free course #10

--- nomad/typechain/src/index.ts
+++ nomad/typechain/src/index.ts
@@ -1,12 +1,6 @@
 import * as CryptoJS from "crypto-js";
 
 class Block {
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
-
     // Block class를 새로 생성하지 않아도 호출 할 수 있음. ex)Block.calculateBlockHash()
     static calculateBlockHash = (
         index: number,
@@ -15,6 +9,19 @@ class Block {
         data: string
     ): string =>
         CryptoJS.SHA256(index + previouseHash + timestamp + data).toString();
+
+    static validateStructure = (aBlock: Block): boolean =>
+        typeof aBlock.index === "number" &&
+        typeof aBlock.hash === "string" &&
+        typeof aBlock.previousHash === "string" &&
+        typeof aBlock.timestamp === "number" &&
+        typeof aBlock.data === "string";
+
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
 
     constructor(
         index: number,
@@ -60,5 +67,14 @@ const createNewBlock = (data: string): Block => {
     return newBlock;
 }
 
-console.log(createNewBlock("Hello"), createNewBlock("bye bye"));
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+}
+
 export { };
