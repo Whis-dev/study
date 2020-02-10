b72222c29226f8cafa32ea2b5da129c72ae231ad
whis <whisdevj@gmail.com>
2020-02-10 15:36:34 +0900

    [nomad>typechain] nomad coders typescript free course #8

--- nomad/typechain/src/index.ts
+++ nomad/typechain/src/index.ts
@@ -1,9 +1,21 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
     public previousHash: string;
     public data: string;
     public timestamp: number;
+
+    // Block class를 새로 생성하지 않아도 호출 할 수 있음. ex)Block.calculateBlockHash()
+    static calculateBlockHash = (
+        index: number,
+        previouseHash: string,
+        timestamp: number,
+        data: string
+    ): string =>
+        CryptoJS.SHA256(index + previouseHash + timestamp + data).toString();
+
     constructor(
         index: number,
         hash: string,
@@ -21,8 +33,12 @@ class Block {
 
 const genesisBlock: Block = new Block(0, "20200202020", "", "Hello", 123456);
 
-let blockchain:[Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
 
-console.log(blockchain);
+const getBlockchain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export { };
