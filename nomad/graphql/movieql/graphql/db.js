1322fd231030b0b346482076f5e0bc40b1ecb77d
whis <whisdevj@gmail.com>
2020-10-18 19:56:14 +0900

    [nomad>graphql>movieql]movie api example use fetch

--- nomad/graphql/movieql/graphql/db.js
+++ nomad/graphql/movieql/graphql/db.js
@@ -1,50 +1,14 @@
-export let movies = [
-    {
-        id: 0,
-        name: "Star Wars - The new one",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "Avengers - The new one",
-        score: 8
-    },
-    {
-        id: 2,
-        name: "The Godfather I",
-        score: 99
-    },
-    {
-        id: 3,
-        name: "Logan",
-        score: 2
-    }
-]
+import fetch from "node-fetch";
+import querystring from 'querystring';
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.am/api/v2/list_movies.json?"
 
-export const getById = id => {
-    return movies.filter(movie => id === movie.id)[0];
-}
+export const getMovies = (limit, minimum_rating) => {
+    const queryData = querystring.stringify({ limit, minimum_rating });
+    const REQUEST_URL = API_URL + queryData;
 
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: movies.length,
-        name,
-        score
-    };
-    movies.push(newMovie);
-    return newMovie;
-}
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies
-        return true;
-    } else{ 
-        return false;
-    }
+    return fetch(REQUEST_URL)
+        .then(res => res.json())
+        .then(json => json.data.movies);
 }
 
--- nomad/graphql/movieql/graphql/resolvers.js
+++ nomad/graphql/movieql/graphql/resolvers.js
@@ -1,19 +1,10 @@
 import { 
-    movies,
-    getById,
-    getMovies,
-    addMovie,
-    deleteMovie
+    getMovies
 } from "./db"
 
 const resolvers = {
     Query: {
-        movies: () => getMovies(),
-        movie: (_, { id }) => getById(id)
-    },
-    Mutation: {
-        addMovie: (_, {name,score}) => addMovie(name,score),
-        deleteMovie: (_, {id}) => deleteMovie(id)
+        movies: (_, {limit,rating}) => getMovies(limit, rating)
     }
 };
 
