a058f5bf153df6d86e16d936b4783c68f0309d25
whis <whisdevj@gmail.com>
2022-06-01 15:45:18 +0900

    [rust > guessing_game] - add shadowing guess type

--- rust/guessing_game/src/main.rs
+++ rust/guessing_game/src/main.rs
@@ -17,11 +17,15 @@ fn main() {
 
 	io::stdin().read_line(&mut guess).expect("Failed to read line");
 
+	// 이전에 있던 값을 가는 것을 허용 -> 하나의 값을 현 타입에서 다른 타입으로 변환하고 싶은 경우 사용.
+	// u32 : 부호가 없는 32비트의 정수
+	let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
 	println!("You guessed: {}", guess);
 
 	match guess.cmp(&secret_number) {
 		Ordering::Less    => println!("Too small!"),
 		Ordering::Greater => println!("Too big!"),
-		Ordering::Equal   => pringln!("You win!")
+		Ordering::Equal   => println!("You win!")
 	}
 }
