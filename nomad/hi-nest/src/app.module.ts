076fe5278e1a914b4da45cedc99a4ffa865e9a43
whis <whisdevj@gmail.com>
2022-07-11 16:59:41 +0900

    [nomad > hi-nest] - delete files for making them

--- nomad/hi-nest/src/app.module.ts
+++ nomad/hi-nest/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 // Decorator : add functionality to class
 @Module({
   imports: [],
-  controllers: [AppController], // like router in Express
-  providers: [AppService],
+  controllers: [], // like router in Express
+  providers: [],
 })
 export class AppModule {}
