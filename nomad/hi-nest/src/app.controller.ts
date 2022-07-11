89a13c0db441d6ddf3a686a6e5fd2cada04e17ab
whis <whisdevj@gmail.com>
2022-07-11 16:58:22 +0900

    [nomad > hi-nest] - explain about module, controller, service

--- nomad/hi-nest/src/app.controller.ts
+++ nomad/hi-nest/src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// getting url and return function(service)
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
+    // why need service??
+    //  because of deviding business logic
     return this.appService.getHello();
+  }
+
+  // like app.get in Express
+  @Get('/hello')
+  sayHello(): string {
+    return this.appService.getHi();
   }
 }
--- nomad/hi-nest/src/app.module.ts
+++ nomad/hi-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// Decorator : add functionality to class
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController], // like router in Express
   providers: [AppService],
 })
 export class AppModule {}
