09f03131802886b13e80ffbbac24f2f4f084976a
whis <whisdevj@gmail.com>
2024-07-26 23:28:13 +0900

    [rust > concurrent] - example of message passing

--- rust/concurrent/src/main.rs
+++ rust/concurrent/src/main.rs
@@ -1,11 +1,15 @@
+use std::sync::mpsc;
 use std::thread;
 
 fn main() {
-    let v = vec![1, 2, 3];
+    let (tx, rx) = mpsc::channel();
 
-    let handle = thread::spawn(move || {
-        println!("Here's a vector: {:?}", v);
+    thread::spawn(move || {
+        let val = String::from("hi");
+
+        tx.send(val).unwrap();
     });
 
-    handle.join().unwrap();
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }
