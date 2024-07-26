5b3cbbef305166ae5564a9780697cb5c6bae620a
whis <whisdevj@gmail.com>
2024-07-26 23:00:19 +0900

    [rust > smart_pointer] - until drop in smart pointer part

---
+++ rust/smart_pointer/src/main.rs
@@ -0,0 +1,53 @@
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+struct CustomSmartPointer {
+    data: String
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self){
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
+fn hello(name: &str) {
+    println!("Hello, {name}!");
+}
+
+fn main(){
+    let x = 5;
+    let y = MyBox::new(x);
+    
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+    
+    let m = MyBox::new(String::from("Rust"));
+    
+    hello(&m);
+    
+    let c = CustomSmartPointer {
+        data: String::from("my stuff")
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff")
+    };
+    println!("CustomSmartPointers created,");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main");
+}
